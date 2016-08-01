059e91846f1f97a9d9f6894fd78a6b25f632a4bd
salvacarrion <salva.carrion@outlook.com>
2016-08-01 02:15:47 +0200

    Enabling CLiMF test

--- orangecontrib/recommendation/tests/test_climf.py
+++ orangecontrib/recommendation/tests/test_climf.py
@@ -12,50 +12,51 @@ import random
 
 class TestCLiMF(unittest.TestCase):
 
+
     def test_CLiMF_input_data_continuous(self):
-        pass
-        # # Load data
-        # data = Orange.data.Table('binary_data.tab')
-        #
-        # # Train recommender
-        # learner = CLiMFLearner(K=2, steps=1, alpha=0.0001, beta=0.001, verbose=True)
-        # recommender = learner(data)
-        # print(str(recommender) + ' trained')
-        #
-        # # Create set to test
-        # num_users = min(recommender.shape[0], 5)
-        # num_items = recommender.shape[1]
-        # test_users = random.sample(range(recommender.shape[0]), num_users)
-        #
-        # # Compute predictions 1
-        # y_pred = recommender(data[test_users], top_k=None)
-        #
-        # # Compute predictions 2 (Execute the 2nd branch)
-        # y_pred2 = recommender(data[test_users].X, top_k=num_items)
-        #
-        # # Compute predictions 3 (Execute the 3rd branch, "no arg")
-        # y_pred3 = recommender(data[test_users], no_real_arg='Something')
-        #
-        # # Get relevant items for the user (to test MRR)
-        # all_items_u = []
-        # for i in test_users:
-        #     items_u = data.X[data.X[:, recommender.order[0]] == i][:,
-        #               recommender.order[1]]
-        #     all_items_u.append(items_u)
-        #
-        # # Compute MRR
-        # mrr = MeanReciprocalRank(results=y_pred, query=all_items_u)
-        # print('-> MRR (input data): %.3f' % mrr)
-        #
-        # # Check tables U and V
-        # U = recommender.getUTable()
-        # V = recommender.getVTable()
-        # self.assertEqual(U.X.shape[1], V.X.shape[1])
-        #
-        # # Check correctness
-        # self.assertGreaterEqual(mrr, 0)
-        # np.testing.assert_equal(y_pred, y_pred2)
-        # np.testing.assert_equal(y_pred, y_pred3)
+
+        # Load data
+        data = Orange.data.Table('binary_data.tab')
+
+        # Train recommender
+        learner = CLiMFLearner(K=2, steps=1, alpha=0.0001, beta=0.001, verbose=True)
+        recommender = learner(data)
+        print(str(recommender) + ' trained')
+
+        # Create set to test
+        num_users = min(recommender.shape[0], 5)
+        num_items = recommender.shape[1]
+        test_users = random.sample(range(recommender.shape[0]), num_users)
+
+        # Compute predictions 1
+        y_pred = recommender(data[test_users], top_k=None)
+
+        # Compute predictions 2 (Execute the 2nd branch)
+        y_pred2 = recommender(data[test_users].X, top_k=num_items)
+
+        # Compute predictions 3 (Execute the 3rd branch, "no arg")
+        y_pred3 = recommender(data[test_users], no_real_arg='Something')
+
+        # Get relevant items for the user (to test MRR)
+        all_items_u = []
+        for i in test_users:
+            items_u = data.X[data.X[:, recommender.order[0]] == i][:,
+                      recommender.order[1]]
+            all_items_u.append(items_u)
+
+        # Compute MRR
+        mrr = MeanReciprocalRank(results=y_pred, query=all_items_u)
+        print('-> MRR (input data): %.3f' % mrr)
+
+        # Check tables U and V
+        U = recommender.getUTable()
+        V = recommender.getVTable()
+        self.assertEqual(U.X.shape[1], V.X.shape[1])
+
+        # Check correctness
+        self.assertGreaterEqual(mrr, 0)
+        np.testing.assert_equal(y_pred, y_pred2)
+        np.testing.assert_equal(y_pred, y_pred3)
 
     def test_CLiMF_input_data_discrete(self):
 
