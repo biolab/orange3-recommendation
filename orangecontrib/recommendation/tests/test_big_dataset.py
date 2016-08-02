128539f772de4c89713d8a40d4e95ef3c5edf782
salvacarrion <salva.carrion@outlook.com>
2016-08-02 17:59:06 +0200

    This is not a unittest anymore

--- orangecontrib/recommendation/tests/test_big_dataset.py
+++ orangecontrib/recommendation/tests/test_big_dataset.py
@@ -1,102 +1,91 @@
 import Orange
+from Orange.evaluation.testing import CrossValidation
 from orangecontrib.recommendation import *
 
 from sklearn.metrics import mean_squared_error
 
-import unittest
 import math
 import time
 
 
-class TestBigDataset(unittest.TestCase):
+def test_learners():
+    start = time.time()
 
-    def test_learners(self):
-        start = time.time()
-
-        # Load data
-        data = Orange.data.Table('movielens100k.tab')
-        print('- Loading time: %.3fs' % (time.time() - start))
+    # Load data
+    data = Orange.data.Table('movielens100k.tab')
+    print('- Loading time: %.3fs' % (time.time() - start))
 
 
-        start = time.time()
-        learner = GlobalAvgLearner()
-        recommender = learner(data)
-        print('- Time (GlobalAvgLearner): %.3fs' % (time.time() - start))
-        rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
-        print('- RMSE (GlobalAvgLearner): %.3f' % rmse)
+    start = time.time()
+    learner = GlobalAvgLearner()
+    recommender = learner(data)
+    print('- Time (GlobalAvgLearner): %.3fs' % (time.time() - start))
+    rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
+    print('- RMSE (GlobalAvgLearner): %.3f' % rmse)
 
-        start = time.time()
-        learner = ItemAvgLearner()
-        recommender = learner(data)
-        print('- Time (ItemAvgLearner): %.3fs' % (time.time() - start))
-        rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
-        print('- RMSE (ItemAvgLearner): %.3f' % rmse)
+    start = time.time()
+    learner = ItemAvgLearner()
+    recommender = learner(data)
+    print('- Time (ItemAvgLearner): %.3fs' % (time.time() - start))
+    rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
+    print('- RMSE (ItemAvgLearner): %.3f' % rmse)
 
-        start = time.time()
-        learner = UserAvgLearner()
-        recommender = learner(data)
-        print('- Time (UserAvgLearner): %.3fs' % (time.time() - start))
-        rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
-        print('- RMSE (UserAvgLearner): %.3f' % rmse)
+    start = time.time()
+    learner = UserAvgLearner()
+    recommender = learner(data)
+    print('- Time (UserAvgLearner): %.3fs' % (time.time() - start))
+    rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
+    print('- RMSE (UserAvgLearner): %.3f' % rmse)
 
-        start = time.time()
-        learner = UserItemBaselineLearner()
-        recommender = learner(data)
-        print('- Time (UserItemBaselineLearner): %.3fs' % (time.time() - start))
-        rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
-        print('- RMSE (UserItemBaselineLearner): %.3f' % rmse)
+    start = time.time()
+    learner = UserItemBaselineLearner()
+    recommender = learner(data)
+    print('- Time (UserItemBaselineLearner): %.3fs' % (time.time() - start))
+    rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
+    print('- RMSE (UserItemBaselineLearner): %.3f' % rmse)
 
-        start = time.time()
-        learner = BRISMFLearner(K=15, steps=15, alpha=0.07, beta=0.1, verbose=False)
-        recommender = learner(data)
-        print('- Time (BRISMFLearner): %.3fs' % (time.time() - start))
-        rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
-        print('- RMSE (BRISMFLearner): %.3f' % rmse)
+    start = time.time()
+    learner = BRISMFLearner(K=15, steps=15, alpha=0.07, beta=0.1, verbose=False)
+    recommender = learner(data)
+    print('- Time (BRISMFLearner): %.3fs' % (time.time() - start))
+    rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
+    print('- RMSE (BRISMFLearner): %.3f' % rmse)
 
-        start = time.time()
-        learner = SVDPlusPlusLearner(K=15, steps=15, alpha=0.07, beta=0.1, verbose=False)
-        recommender = learner(data)
-        print('- Time (SVDPlusPlusLearner): %.3fs' % (time.time() - start))
-        rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
-        print('- RMSE (SVDPlusPlusLearner): %.3f' % rmse)
-
-        self.assertEqual(1, 1)
+    start = time.time()
+    learner = SVDPlusPlusLearner(K=15, steps=15, alpha=0.07, beta=0.1, verbose=False)
+    recommender = learner(data)
+    print('- Time (SVDPlusPlusLearner): %.3fs' % (time.time() - start))
+    rmse = math.sqrt(mean_squared_error(data.Y, recommender(data)))
+    print('- RMSE (SVDPlusPlusLearner): %.3f' % rmse)
 
 
-    def test_CV(self):
-        from Orange.evaluation.testing import CrossValidation
-        # Load data
-        data = Orange.data.Table('filmtrust/ratings_small.tab')
-        trust = Orange.data.Table('filmtrust/trust_small.tab')
+def test_CV():
 
-        global_avg = GlobalAvgLearner()
-        items_avg = ItemAvgLearner()
-        users_avg = UserAvgLearner()
-        useritem_baseline = UserItemBaselineLearner()
-        brismf = BRISMFLearner(K=15, steps=10, alpha=0.07, beta=0.1)
-        svdpp = SVDPlusPlusLearner(K=15, steps=10, alpha=0.007, beta=0.1)
-        trustsvd = TrustSVDLearner(K=15, steps=10, alpha=0.007, beta=0.1,
-                                   beta_trust=0.05, trust=trust)
-        learners = [global_avg, items_avg, users_avg, useritem_baseline,
-                    brismf, svdpp, trustsvd]
+    # Load data
+    data = Orange.data.Table('filmtrust/ratings_small.tab')
+    trust = Orange.data.Table('filmtrust/trust_small.tab')
 
-        res = CrossValidation(data, learners, k=5)
-        rmse = Orange.evaluation.RMSE(res)
-        r2 = Orange.evaluation.R2(res)
+    # Learners
+    global_avg = GlobalAvgLearner()
+    items_avg = ItemAvgLearner()
+    users_avg = UserAvgLearner()
+    useritem_baseline = UserItemBaselineLearner()
+    brismf = BRISMFLearner(K=15, steps=10, alpha=0.07, beta=0.1)
+    svdpp = SVDPlusPlusLearner(K=15, steps=10, alpha=0.007, beta=0.1)
+    trustsvd = TrustSVDLearner(K=15, steps=10, alpha=0.007, beta=0.1,
+                               beta_trust=0.05, trust=trust)
+    learners = [global_avg, items_avg, users_avg, useritem_baseline,
+                brismf, svdpp, trustsvd]
 
-        print("Learner  RMSE  R2")
-        for i in range(len(learners)):
-            print(
-                "{:8s} {:.2f} {:5.2f}".format(learners[i].name, rmse[i], r2[i]))
+    res = CrossValidation(data, learners, k=5)
+    rmse = Orange.evaluation.RMSE(res)
+    r2 = Orange.evaluation.R2(res)
 
-        self.assertEqual(1, 1)
+    print("Learner  RMSE  R2")
+    for i in range(len(learners)):
+        print(
+            "{:8s} {:.2f} {:5.2f}".format(learners[i].name, rmse[i], r2[i]))
 
 if __name__ == "__main__":
-    # Test all
-    # unittest.main()
-
-    # Test single test
-    suite = unittest.TestSuite()
-    suite.addTest(TestBigDataset("test_learners"))
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+    pass
+    test_learners()
